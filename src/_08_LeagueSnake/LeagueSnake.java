4d600fde1b7d1195b705db1b0d66927953900271
league <league@imac-14.attlocal.net>
2022-12-21 19:57:15 -0800

    Worked on Snake game

--- src/_08_LeagueSnake/LeagueSnake.java
+++ src/_08_LeagueSnake/LeagueSnake.java
@@ -11,7 +11,9 @@ public class LeagueSnake extends PApplet {
      * 
      * Put all the game variables here.
      */
-    
+    Segment head;
+    int foodX;
+    int foodY;
 
     
     /*
@@ -21,17 +23,20 @@ public class LeagueSnake extends PApplet {
      */
     @Override
     public void settings() {
-        
+        size(500,500);
     }
 
     @Override
     public void setup() {
-        
+        head = new Segment(250,250);
+        frameRate(20);
+        dropFood();
     }
 
     void dropFood() {
         // Set the food in a new random location
-        
+    	foodX = ((int)random(50)*10);
+    	foodY = ((int)random(50)*10);
     }
 
     /*
@@ -42,16 +47,20 @@ public class LeagueSnake extends PApplet {
 
     @Override
     public void draw() {
-        
+        background(0,0,0);
+        drawFood();
+        drawSnake();
     }
 
     void drawFood() {
         // Draw the food
-        
+        rect(10,10,foodX,foodY);
+        color(255,0,0);
     }
 
     void drawSnake() {
         // Draw the head of the snake followed by its tail
+    	
     }
 
     void drawTail() {
